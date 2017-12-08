7788dce8fe472dd376f8e125931bf9602c91c3f2
Takashi SASAKI <takashi316@gmail.com>
2017-12-08 14:10:48 +0900

    fix bugs sqlite{2|3}restore

--- test/tests.sh
+++ test/tests.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-../scripts/sqlite2restore <test.sqlite2.dump | ../scripts/sqlite2dump >test.sqlite2.dump2
+echo testing sqlite2restore
+../scripts/sqlite2restore <test.sqlite2.dump >temp.sqlite2
+echo testing sqlite2dump
+ ../scripts/sqlite2dump >test.sqlite2.dump2 <temp.sqlite2
+echo diff
 diff test.sqlite2.dump test.sqlite2.dump2
-../scripts/sqlite3restore <test.sqlite3.dump | ../scripts/sqlite3dump >test.sqlite3.dump2
+echo testing sqlite3restore
+../scripts/sqlite3restore <test.sqlite3.dump >temp.sqlite3
+echo testing sqlite3dump
+../scripts/sqlite3dump >test.sqlite3.dump2 <temp.sqlite3
+echo diff
 diff test.sqlite3.dump test.sqlite3.dump2
 
