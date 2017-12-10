d58e96a90d49b6d0badfe39affe9e18bdc3ba0fa
Takashi SASAKI <takashi316@gmail.com>
2017-12-10 18:53:16 +0900

    git-sqlite-0.2.0

---
+++ git-sqlite-0.2.0/test/tests.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo testing sqlite2restore
+../scripts/sqlite2restore <test.sqlite2.dump >temp.sqlite2
+echo testing sqlite2dump
+ ../scripts/sqlite2dump >test.sqlite2.dump2 <temp.sqlite2
+echo diff
+diff test.sqlite2.dump test.sqlite2.dump2
+echo testing sqlite3restore
+../scripts/sqlite3restore <test.sqlite3.dump >temp.sqlite3
+echo testing sqlite3dump
+../scripts/sqlite3dump >test.sqlite3.dump2 <temp.sqlite3
+echo diff
+diff test.sqlite3.dump test.sqlite3.dump2
+echo testing sqlite2restore for malformed input
+../scripts/sqlite2restore <test.sqlite2.dump.malformed >temp.sqlite2.dump.malformed
+echo diff
+diff test.sqlite2.dump.malformed temp.sqlite2.dump.malformed
+echo testing sqlite3restore for malformed input
+../scripts/sqlite3restore <test.sqlite3.dump.malformed >temp.sqlite3.dump.malformed
+echo diff
+diff test.sqlite3.dump.malformed temp.sqlite3.dump.malformed
